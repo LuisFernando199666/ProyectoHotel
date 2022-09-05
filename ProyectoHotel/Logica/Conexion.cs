5c22fc683c55f9c3459e02d9876793249a89a8bb
LuisFernando199666 <marinlizcanosena@gmail.com>
2022-09-04 19:09:41 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoHotel/Logica/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoHotel.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "workstation id=DBHotel2.mssql.somee.com;packet size=4096;user id=LUISLIZCANO1996A_SQLLogin_1;pwd=lpzgts2kvf;data source=DBHotel2.mssql.somee.com;persist security info=False;initial catalog=DBHotel2";
+        //public static string CN = "Data Source=DESKTOP-KHFR34C\\SQLEXPRESS;Initial Catalog=DB_HOTEL;Integrated Security=True";
+    }
+}
